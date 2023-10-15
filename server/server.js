20ee8e709363c027dd75e9975d5aeabfb70b7bb7
IngkamonCH <s6404062630601@email.kmutnb.ac.th>
2023-10-15 18:02:47 +0700

    add server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require("cors");
+const db = require("./database");
+
+const login = require("./login.js");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.listen(5000, ()=>{
     console.log(`Server is running on port 5000`)
 });
+
+//test
+// app.get("/api", (req, res) => {
+//     res.json({"user": ["userone","usertwo"]});
+// })
+
+app.get("/admin",(req, res)=>{
+    db.query("SELECT * FROM admin", (err, result)=> {
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result)
+        }
+    })
+})
+
+app.use("/", login);
